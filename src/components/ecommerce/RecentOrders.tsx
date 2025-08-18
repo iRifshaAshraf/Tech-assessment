b73cfc4399fa7e1265804ecd7ba97cdce174e4d8
neajmorshed0 <neajmorshedimon@gmail.com>
2025-08-18 18:15:40 +0100

    update RecentOrders.tsx

--- src/components/ecommerce/RecentOrders.tsx
+++ src/components/ecommerce/RecentOrders.tsx
@@ -12,9 +12,8 @@ interface Product {
   id: number; // Unique identifier for each product
   name: string; // Product name
   variants: string; // Number of variants (e.g., "1 Variant", "2 Variants")
-  category: string; // Category of the product
+  category:  string; 
   price: string; // Price of the product (as a string with currency symbol)
-  // status: string; // Status of the product
   image: string; // URL or path to the product image
   status: "Delivered" | "Pending" | "Canceled"; // Status of the product
 }
@@ -23,21 +22,21 @@ interface Product {
 const tableData: Product[] = [
   {
     id: 1,
-    name: "MacBook Pro 13”",
+    name: 'MacBook Pro 13”', 
     variants: "2 Variants",
     category: "Laptop",
     price: "$2399.00",
     status: "Delivered",
-    image: "/images/product/product-01.jpg", // Replace with actual image URL
+    image: "/images/product/product-01.jpg", 
   },
   {
     id: 2,
-    name: "Apple Watch Ultra",
-    variants: "1 Variant",
-    category: "Watch",
-    price: "$879.00",
+    name: "Apple Watch Ultra", 
+    variants: "1 Variant", 
+    category: "Watch", 
+    price: "$879.00", 
     status: "Pending",
-    image: "/images/product/product-02.jpg", // Replace with actual image URL
+    image: "/images/product/product-02.jpg", 
   },
   {
     id: 3,
@@ -46,31 +45,32 @@ const tableData: Product[] = [
     category: "SmartPhone",
     price: "$1869.00",
     status: "Delivered",
-    image: "/images/product/product-03.jpg", // Replace with actual image URL
+    image: "/images/product/product-03.jpg",   
   },
   {
     id: 4,
     name: "iPad Pro 3rd Gen",
     variants: "2 Variants",
-    category: "Electronics",
+    category: "Electronics", 
     price: "$1699.00",
-    status: "Canceled",
-    image: "/images/product/product-04.jpg", // Replace with actual image URL
+    status: "Canceled", 
+    image: "/images/product/product-04.jpg", 
   },
   {
     id: 5,
     name: "AirPods Pro 2nd Gen",
     variants: "1 Variant",
-    category: "Accessories",
-    price: "$240.00",
-    status: "Delivered",
-    image: "/images/product/product-05.jpg", // Replace with actual image URL
+    category: "Accessories",  
+    price: "$240.00",  
+    status: "Delivered", 
+    image: "/images/product/product-05.jpg", 
   },
 ];
 
 export default function RecentOrders() {
   return (
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
+      {/* Header */} 
       <div className="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
@@ -78,6 +78,7 @@ export default function RecentOrders() {
           </h3>
         </div>
 
+        {/* Buttons  */}
         <div className="flex items-center gap-3">
           <button className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
             <svg
@@ -95,7 +96,7 @@ export default function RecentOrders() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
               />
-              <path
+              <path 
                 d="M17.7075 14.0961H2.29085"
                 stroke=""
                 strokeWidth="1.5"
@@ -122,6 +123,8 @@ export default function RecentOrders() {
           </button>
         </div>
       </div>
+
+      {/* Table  */}
       <div className="max-w-full overflow-x-auto">
         <Table>
           {/* Table Header */}
@@ -155,17 +158,16 @@ export default function RecentOrders() {
           </TableHeader>
 
           {/* Table Body */}
-
           <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
             {tableData.map((product) => (
-              <TableRow key={product.id} className="">
+              <TableRow key={product.id}>
                 <TableCell className="py-3">
                   <div className="flex items-center gap-3">
                     <div className="h-[50px] w-[50px] overflow-hidden rounded-md">
                       <img
                         src={product.image}
-                        className="h-[50px] w-[50px]"
                         alt={product.name}
+                        className="h-[50px] w-[50px]"
                       />
                     </div>
                     <div>
@@ -206,3 +208,4 @@ export default function RecentOrders() {
     </div>
   );
 }
+ 
